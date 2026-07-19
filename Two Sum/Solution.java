0449cd6acd691ebb4cba95b120e763c073cef95b
Ghost <thrustvectoringghost@gmail.com>
2026-07-19 18:14:07 -0400

    Two Sum

---
+++ Two Sum/Solution.java
@@ -0,0 +1,21 @@
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] combo = new int[2];
+  
+        for(int i = 0 ; i < nums.length; i++){
+            for(int n = 0 ; n < nums.length; n++){
+                if(i == n){
+                    continue;
+                }
+                if(nums[i] + nums[n] == target){
+                    combo[0] = i;
+                    combo[1] = n;
+                    return combo;
+                }
+            }
+        }
+        return combo;
+    }
+}
